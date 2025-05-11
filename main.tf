f2a49694cdf551b82b9e5627665c63d44e53c5f4
yanggautier <yangguole@outlook.com>
2025-05-11 22:45:55 +0200

    remove extra server and refractor outputs

--- main.tf
+++ main.tf
@@ -107,7 +107,7 @@ resource "aws_internet_gateway" "internet_gateway" {
 
 #Create EIP for NAT Gateway
 resource "aws_eip" "nat_gateway_eip" {
-  domain     = "vpc"
+ #  domain     = "vpc"
   depends_on = [aws_internet_gateway.internet_gateway]
   tags = {
     Name = "demo_igw_eip"
@@ -144,44 +144,6 @@ data "aws_ami" "ubuntu" {
   }
 
   owners = ["099720109477"]
-}
-
-# Terraform Resource Block - To Build EC2 instance in Public Subnet
-resource "aws_instance" "ubuntu_server" {
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.public_subnets["public_subnet_1"].id
-  security_groups             = [aws_security_group.vpc-ping.id, aws_security_group.ingress-ssh.id, aws_security_group.vpc-web.id]
-  associate_public_ip_address = true
-  key_name                    = aws_key_pair.generated.key_name
-  connection {
-    user        = "ubuntu"
-    private_key = tls_private_key.generated.private_key_pem
-    host        = self.public_ip
-  }
-
-  # Leave the first part of the block unchanged and create our `local-exec` provisioner
-  # provisioner "local-exec" {
-  #   command = "chmod 600 ${local_file.private_key_pem.filename}"
-  # }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo rm -rf /tmp",
-      "sudo git clone https://github.com/hashicorp/demo-terraform-101 /tmp",
-      "sudo sh /tmp/assets/setup-web.sh",
-    ]
-  }
-
-  tags = {
-    Name = "Ubuntu EC2 Server"
-  }
-
-  lifecycle {
-    ignore_changes = [security_groups]
-  }
-
-
 }
 
 # Terraform Resource Block - Security Group to Allow Ping Traffic
@@ -313,14 +275,6 @@ resource "aws_instance" "web_server_2" {
   tags = {
     Name = "Web EC2 Server 2"
   }
-}
-
-output "public_ip" {
-  value = aws_instance.ubuntu_server.public_ip
-}
-
-output "public_dns" {
-  value = aws_instance.ubuntu_server.public_dns
 }
 
 output "public_ip_server_subnet_1" {
